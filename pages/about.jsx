349f0b502aebe2d75f82482bbece1515c00007fe
rayGreycloud <raygreycloud@gmail.com>
2021-08-03 07:33:49 -0700

    initial commit

---
+++ pages/about.jsx
@@ -0,0 +1,14 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        temporibus dolorem dicta.
+      </p>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+};
+
+export default AboutPage;
